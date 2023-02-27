f9203b99bc8224fe70211bd04753e5555ccb3682
zzse <tpgus0274@gmail.com>
2023-02-27 22:29:46 +0900

    세현 테스트

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -5,5 +5,8 @@ public class HomeController {
     public static void main(String[] args){
         System.out.println("안녕하세요");
         System.out.println("바보~~~");
+        System.out.println("다현이 지현이는 천재만세");
+
+
     }
 }
