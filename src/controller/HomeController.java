872953bbb751bfdef4a1107f133c00f1a2988a29
onsundayn <onsundayn@gmail.com>
2023-02-27 22:30:18 +0900

    2023.02.27 권다현 TEST

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -4,9 +4,9 @@ public class HomeController {
 
     public static void main(String[] args){
         System.out.println("안녕하세요");
-        System.out.println("바보~~~");
+
         System.out.println("다현이 지현이는 천재만세");
-
-
+        
+        System.out.println("히히");
     }
 }
